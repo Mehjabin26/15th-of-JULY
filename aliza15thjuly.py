12c51c5796660bef23e9c343f9495eff53ecb202
Tamanna <tmahjabin2020@gmail.com>
2025-07-15 18:10:49 +0600

    sent it

---
+++ aliza15thjuly.py
@@ -0,0 +1,28 @@
+from pycparser.c_ast import Continue
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from faker import Faker
+fake=Faker()
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://tutorialsninja.com/demo/index.php?route=account/register")
+time.sleep(18)
+FirstName = driver.find_element(by=By.ID, value="input-firstname")
+FirstName.send_keys("TAMANNA")
+LastName = driver.find_element(by=By.ID, value="input-lastname")
+LastName.send_keys("MAHJABIN")
+E_mail = driver.find_element(by=By.ID, value="input-email")
+E_mail.send_keys("tmahjabin@gmail.com")
+Telephone = driver.find_element(by=By.ID, value="input-telephone")
+Telephone.send_keys("01904477000")
+Password = driver.find_element(by=By.ID, value="input-password")
+Password.send_keys("123456")
+PasswordConfirm = driver.find_element(by=By.ID, value="input-confirm")
+PasswordConfirm.send_keys("123456")
+Subscribe = driver.find_element(by=By.NAME, value="newsletter")
+Subscribe.click()
+Privacy_Policy = driver.find_element(by=By.NAME, value="agree")
+Privacy_Policy.click()
+Continue = driver.find_element(by=By.CSS_SELECTOR, value="input.btn")
+Continue.click()
